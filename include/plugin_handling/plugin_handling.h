b6123aeb3a5bad779d8178ba4f8e1f9a4aec4287
João Pedro <joao.pedro@engenharia.ufjf.br>
2021-06-30 17:24:13 +0100

    First functional version. Complete Wrapper from PSDK. Support to dynamic and static plugin loaders

---
+++ include/plugin_handling/plugin_handling.h
@@ -0,0 +1,8 @@
+//
+// Created by joaopedro on 30/06/21.
+//
+
+#ifndef PLUGIN_TEST_V1_PLUGIN_HANDLING_H
+#define PLUGIN_TEST_V1_PLUGIN_HANDLING_H
+
+#endif //PLUGIN_TEST_V1_PLUGIN_HANDLING_H
---
+++ src/plugin_handling/plugin_handling.cpp
@@ -0,0 +1,4 @@
+//
+// Created by joaopedro on 30/06/21.
+//
+
